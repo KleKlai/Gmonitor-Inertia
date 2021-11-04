613fd15d9da5fd1ee1032b129308f012ef43c5d9
pnavarez <navarezphilip@gmail.com>
2021-11-05 01:07:48 +0800

    laravel broadcasting process

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('question.{classroomId}', function ($user, $classroomId) {
+    if (Auth::check()) {
+        return [
+            'user_id' => $user->id,
+            'user_name' => $user->name,
+            'classroom_id' => $classroomId,
+        ];
+    }
 });
